45e71268137e048d5a19002b05bfa9f0ec12b691
pablo perez <perezpablo0903mail.com>
2023-09-14 16:11:46 -0300

    actualizacion puerto

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const loadDB = require("./loadDB");
-const PORT = 3001;
+const port = process.env.PORT || 3001;
 
 conn.sync({ force: true }).then(async () => {
   await loadDB();
   server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${port}`);
   })
 }).catch(error => console.error(error))
