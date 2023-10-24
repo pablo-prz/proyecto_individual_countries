be7598b75c639e583e2e59313c2d0639ea356c3a
pablo perez <perezpablo0903mail.com>
2023-10-24 16:03:25 -0300

    dasd

--- server/src/db.js
+++ server/src/db.js
@@ -3,11 +3,11 @@ const { Sequelize } = require("sequelize");
 
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+import {
+  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+} from process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false,
   native: false,
 });
