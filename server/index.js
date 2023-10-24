b1d3ae24c7a0f1238a29a547c4544a081409ac16
pablo perez <perezpablo0903mail.com>
2023-10-24 20:47:43 -0300

    try 1234

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const { conn } = require('./src/db.js');
 const loadDB = require("./loadDB");
 const port = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(async () => {
+conn.sync({ force: false }).then(async () => {
   await loadDB();
   server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   })
-}).catch(error => console.error(error))
+}).catch(error => console.error("Error syncing database:", error))
